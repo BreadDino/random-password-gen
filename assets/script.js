ede65b9a90c86ce9d7e8c060ab101ff190dffd81
unknown <gabemarredondo@gmail.com>
2022-08-21 13:31:15 -0700

    switched variables from write password to generate password

--- assets/script.js
+++ assets/script.js
@@ -3,41 +3,54 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 // different user choices
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var numbers = "1234567890".split("");
-var specialChar = "!@#$%&'()*+,-./:;<=>?@[]^_`{|}~]".split("");
-var allArray = [specialChar, upperCase, lowerCase, numbers];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialChar = "!@#$%&'()*+,-./:;<=>?@[]^_`{|}~]";
 
 // Write password to the #password input
-function writePassword() {
-  // asks user to chose what they want to include
-  var InLowercase = confirm("include lower case?");
-  var InUppercase = confirm("include upper case?");
-  var InNumbers = confirm("include numbers?");
-  var InSpecial = confirm("include special characters?");
-  
-  // activates pasword generator
-  var password = generatePassword();
+function generatePassword() {
+  // empty variable to hold all of the choices the user wants
+  var possibleChars = "";
 
-  passwordText.textContent = password;
-
-  function generatePassword() {
-
-    if (InLowercase === false) {
-      lowerCase = "";
+  var passwordLength = parseInt(
+    prompt(
+      "How long would you like your password? please choose between 8 and 128."
+    )
+  );
+  // makes sure the password length is within the min and maximum
+  if (
+    passwordLength < minChars ||
+    passwordLength > maxChars ||
+    isNaN(passwordLength)
+  ) {
+    // asks users what types of characters they would like in there password
+    var InLowercase = confirm("Would you like to include lower case?");
+    var InUppercase = confirm("Would you like to include upper case?");
+    var InNumbers = confirm("Would you like to include numbers?");
+    var InSpecial = confirm("Would you like to include special characters?");
+    if (InLowercase) {
+      password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     }
-    if (InUppercase === false) {
-      upperCase = "";
+    if (InUppercase) {
+      password += upperCase[Math.floor(Math.random() * upperCase.length)];
     }
-    if (InNumbers === false) {
-      numbers = "";
+    if (InSpecial) {
+      password += specialChar[Math.floor(Math.random() * specialChar.length)];
     }
-    if (InSpecial === false) {
-      specialChar = "";
+    if (InNumbers) {
+      password += numbers[Math.floor(Math.random() * numbers.length)];
     }
-    console.log(allArray)
   }
 }
-// Add event listener to generate button
+function writePassword() {
+  // generates and writes the password with all the different choices they want to include
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  // activates pasword generator
+}
+
 generateBtn.addEventListener("click", writePassword);
